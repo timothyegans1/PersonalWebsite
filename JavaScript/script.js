9c8b768a77f6bff295be0650500678094e40b041
Timothy Egans Jr <TimothyEgans@gmail.com>
2023-09-24 13:22:16 -0500

    Added placeholder content for Project 1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,15 @@
 <script src="https://kit.fontawesome.com/22b1aec6e8.js" crossorigin="anonymous"></script>
+
+const textElement = document.getElementById('text');
+const textToType = "Hello World!";
+let index = 0;
+
+function typeText() {
+    if (index < textToType.length) {
+        textElement.textContent += textToType.charAt(index);
+        index++;
+        setTimeout(typeText, 200); // Adjust the speed of typing by changing this value
+    }
+}
+
+typeText();
